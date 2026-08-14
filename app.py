1b27a2bc6b04a6742085efec91a6b77a32cd5d96
koteswarn209-hash <koteswarn209@gmail.com>
2026-08-14 23:19:02 +0530

    Update greeting message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world how are you"
+    return "Hello world how are you i am fine"
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
