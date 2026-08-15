16c30ba31afb221c5677936e1361672e42ac24ea
koteswarn209-hash <koteswarn209@gmail.com>
2026-08-15 12:42:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world this is my first project"
+    return "Hello world this is my first projects"
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
